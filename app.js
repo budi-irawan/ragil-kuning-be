8d81d8d56af976c4572f9a16c8fe772d7a7cc348
budi-irawan <budi.irawan171717@gmail.com>
2023-06-20 23:19:39 +0700

    cors

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var app = express();
 var path = require('path');
 var logger = require('morgan');
 const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:8080', 
+const corsOptions = {
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
